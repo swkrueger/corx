8d8de07acc540c9006a652b36a974d83ffdd8f40
Schalk-Willem Krüger <swk@swk.za.net>
2017-03-02 12:43:04 +0200

    Capture scripts: use corx_rx in build dir

--- capture_parallel.sh
+++ capture_parallel.sh
@@ -4,5 +4,5 @@ set -e
 
 source settings.sh
 DATE=$(date +%Y-%m-%d_%H-%M-%S)
-parallel --output-as-files ./corx_rx ${PARAMS} -o ${OUTPUT_PATH}/${DATE}_rx{}.corx -d {} ::: 0 1 2 3
+parallel --output-as-files build/corx_rx ${PARAMS} -o ${OUTPUT_PATH}/${DATE}_rx{}.corx -d {} ::: 0 1 2 3
 # Alternatives: --tmux or --line-buffer --tagstring {}
--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -e
 
 source settings.sh
 rtl_biast -b 1 "$@"
-./corx_rx ${PARAMS} "$@"
+build/corx_rx ${PARAMS} "$@"
